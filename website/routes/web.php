458edc61335e5794e4709d04167a25fccd3b53c0
DKlemczak <73554778+DKlemczak@users.noreply.github.com>
2021-03-18 14:57:00 +0100

    ogólny rozwój

--- website/routes/web.php
+++ website/routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','App\Http\Controllers\StaticController@index')->name('index');
 
+Route::get('/products','App\Http\Controllers\ProductController@index')->name('products');
+Route::get('/products/{id}','App\Http\Controllers\ProductController@details')->name('products.details');
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

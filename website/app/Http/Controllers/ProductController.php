465b982771c93b1d961d273ca46afadaad0b9abb
DKlemczak <73554778+DKlemczak@users.noreply.github.com>
2021-03-29 14:00:17 +0200

    Rekurencja od grup produktów
    - Refakturyzacja skryptu odpowiedzialnego za zbieranie produktów podrzędnych grup

--- website/app/Http/Controllers/ProductController.php
+++ website/app/Http/Controllers/ProductController.php
@@ -10,24 +10,26 @@ class ProductController extends Controller
 {
     function index($id)
     {
-        $product_groups = Products_Group::where('id',$id)->first();
+        $products_group = Products_Group::where('id',$id)->first();
+        $groups_array = array();
+        $groups_array = $this->find_groups($products_group, $groups_array);
 
-        if($product_groups->Products_Group->count() != 0)
-        {
-            $groups_array = array();
-            $counter = 0;
-            foreach($product_groups->Products_Group as $Product_group)
-            {
-                $groups_array[$counter] = $Product_group->id;
-                $counter++;
-            }
-            $products = Products::whereIn('group_id',$groups_array)->get();
-        }
-        else
-        {
-            $products = Products::where('group_id',$id)->where('is_active',1)->get();
-        }
+        $products = Products::whereIn('group_id',$groups_array)->where('is_active',1)->get();
+
         return view('products.index',['products'=>$products]);
+    }
+
+    function find_groups($group, $groups_array)
+    {
+        if($group->Products_Group->count() != 0)
+        {
+            foreach($group->Products_Group as $child_group)
+            {
+                $groups_array = $this->find_groups($child_group, $groups_array);
+            }
+        }
+        $groups_array[$group->id] = $group->id;
+        return $groups_array;
     }
 
     function details($group_id, $id)

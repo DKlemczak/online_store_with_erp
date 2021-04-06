de57550f8e89da3bf9cf27bd7ac40601adc690c1
GARcraft <pawlik616@gmail.com>
2021-04-06 22:39:09 +0200

    Stworzenie dodatkowej karty "Edycja cech towaru" na widoku towaru.

---
+++ enova/enova365.OnlineStoreWithErp/enova365.OnlineStoreWithErp/enova365.OnlineStoreWithErp.UI/ExtViews/TowarExtView.cs
@@ -0,0 +1,14 @@
+﻿using enova365.OnlineStoreWithErp.UI.ExtViews;
+using Soneta.Business;
+
+[assembly: Worker(typeof(TowarExtView))]
+
+namespace enova365.OnlineStoreWithErp.UI.ExtViews
+{
+    public class TowarExtView : ContextBase
+    {
+        public TowarExtView(Context cx) : base(cx) { }
+
+        public string Komunikat { get; set; } = "W tym miejscu będzie widok cech z możliwością ich edycji.";
+    }
+}

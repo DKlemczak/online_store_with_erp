31fb8cb1578ce53eaaacbede04a64edfcebe85a9
GARcraft <pawlik616@gmail.com>
2021-04-06 22:23:16 +0200

    Stworzenie przycisku "Synchronizuj zamówienia" na karcie dokumentów handlowych

---
+++ enova/enova365.OnlineStoreWithErp/enova365.OnlineStoreWithErp/enova365.OnlineStoreWithErp/Workers/SynchronizujZamowieniaWorker.cs
@@ -0,0 +1,11 @@
+﻿using Soneta.Handel;
+using System;
+
+namespace enova365.OnlineStoreWithErp.Workers
+{
+    public class SynchronizujZamowieniaWorker
+    {
+        public static object SynchronizujZamowieniaMethod(DokumentHandlowy[] dokumenty)
+            => throw new NotImplementedException("Metoda \"Synchronizuj zamówienia\" nie została jeszcze zaimplementowana.");
+    }
+}

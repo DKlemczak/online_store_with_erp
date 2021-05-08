6bbd3295607b01c561af512df39fec16de503711
Paulina <80464743+testowa-pau@users.noreply.github.com>
2021-05-08 23:38:43 +0200

    Przebudowa stopki
    - nowy wygląd,
    - dodanie sekcji kontakt oraz logo sklepu

--- website/resources/views/layouts/app.blade.php
+++ website/resources/views/layouts/app.blade.php
@@ -100,13 +100,56 @@
             @yield('content')
         </main>
     </div>
-    <footer>
-      <div class="footer-text">
-        <ul class="mb-0">
-            <li>  <a href="https://www.facebook.com/" target="_blank"> <i class="fab fa-facebook-square"></i> </a> </li>
-            <li> <a href="https://www.instagram.com/" target="_blank"> <i class="fab fa-instagram-square"></i> </a> </li>
-        </ul>
-    </div>
-    </footer>
+
+    <div class="container-fluid pb-0 mb-0 justify-content-center text-light">
+     <footer>
+         <div class="row my-5 justify-content-center py-5">
+             <div class="col-11">
+                 <div class="row">
+                     <div class="col-xl-8 col-md-4 col-sm-4 col-12 my-auto mx-auto a">
+                    <img src="..\img\logo.png" width="150px">
+                     </div>
+
+                     <div class="col-xl-2 col-md-4 col-sm-4 col-12">
+                         <h6 class="mb-3 mb-lg-4 bold-text "><b>MENU</b></h6>
+                         <ul class="list-unstyled">
+                             <li>Home</li>
+                             <li>...</li>
+                             <li>...</li>
+                         </ul>
+                     </div>
+
+                     <div class="col-xl-2 col-md-4 col-sm-4 col-12">
+                         <h6 class="mb-3 mb-lg-4 text-muted bold-text mt-sm-0 mt-5"><b>Siedziba firmy:</b></h6>
+                         <p class="mb-1">ul. Przykładowa 12, </p>
+                         <p> 00-111 Poznań</p>
+                     </div>
+                 </div>
+
+                 <div class="row">
+                     <div class="col-xl-8 col-md-4 col-sm-4 col-auto my-md-0 mt-5 order-sm-1 order-3 align-self-end">
+                         <p class="social text-muted mb-0 pb-0 bold-text"> 
+                             <!-- <span class="mx-2"><i class="fa fa-facebook" aria-hidden="true"></i></span>  -->
+                             <span class="mx-2"> <a href="https://www.facebook.com/" target="_blank"> <i class="fab fa-facebook-square"> </i> </a> </span>
+                             <span class="mx-2"> <a href="https://www.linkedin.com/" target="_blank"> <i class="fa fa-linkedin-square" aria-hidden="true"> </i> </a> </span> 
+                             <span class="mx-2"> <a href="https://www.twitter.com/" target="_blank"> <i class="fa fa-twitter" aria-hidden="true"> </i> </a> </span> 
+                             <span class="mx-2"> <a href="https://www.instagram.com/" target="_blank"> <i class="fa fa-instagram" aria-hidden="true"> </i> </a> </span> 
+                            </p>
+                            <small class="rights"><span>&#174;</span> <strong> funky-szklanki </strong> All Rights Reserved.</small>
+                     </div>
+                     <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
+                         <h6 class="mt-55 mt-2 text-muted bold-text"><b>Paulina Wróblewska</b></h6><small> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> pw@funky-szklanki.com</small>
+                     </div>
+                     <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-2 align-self-end mt-3 ">
+                         <h6 class="text-muted bold-text"><b>Paweł Ganczarski</b></h6><small><span><i class="fa fa-envelope" aria-hidden="true"></i></span> pg@funky-szklanki.com</small>
+                     </div>
+                     <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
+                         <h6 class="mt-55 mt-2 text-muted bold-text"><b>Daniel Klemczak </b></h6><small> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> dk@funky-szklanki.com</small>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </footer>
+ </div>
 </body>
 </html>

43f52950d2393a0fcfe3c69d4493290130fbba2c
DKlemczak <73554778+DKlemczak@users.noreply.github.com>
2021-06-08 15:17:11 +0200

    Usuwanie zdjęć
    Do zrobienia - dodawanie zdjęć i zapisywanie ich w folderze w public

--- website/app/Http/Controllers/Dashboard/ProductPhotosController.php
+++ website/app/Http/Controllers/Dashboard/ProductPhotosController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product_Photos;
 
 class ProductPhotosController extends Controller
 {
     function index($id)
     {
+        $photos = Product_Photos::where('product_id',$id)->get();
+        return view('dashboard.products.photos.index',['photos' => $photos]);
+    }
 
+    function destroy(Request $request,$id)
+    {
+        $photo = Product_Photos::where('id',$id)->first();
+        $product_id = $photo->product_id;
+        $photo->delete();
+
+        $photos = Product_Photos::where('product_id',$product_id)->get();
+        return redirect()->route('dashboard.products.photos.index',[$product_id]);
     }
 }
--- website/resources/views/dashboard/products/photos/index.blade.php
+++ website/resources/views/dashboard/products/photos/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container text-center">
     <div class="row">
         <div class="col-1">
-            <p>Liczba porządkowa</p>
+            <p>Lp.</p>
         </div>
         <div class="col-6">
             <p>Zdjęcie</p>
@@ -18,8 +18,22 @@
     </div>
     @foreach($photos as $photo)
     <div class="row">
-        <div class="col-2">
-            <a href="route"><button type="button" class="btn btn-danger">Usuń zdjęcie</button></a>
+        <div class="col-1 justify-content-center" style="display: flex;">
+            <p style="align-self: center" class="m-0">{!!$photo->no!!}</p>
+        </div>
+        <div class="col-6 justify-content-center" style="display: flex;">
+            <p style="align-self: center" class="m-0">Zdjęcie</p>
+        </div>
+        <div class="col-3 justify-content-center" style="display: flex;">
+            <p style="align-self: center" class="m-0">{!!$photo->path!!}</p>
+        </div>
+        <div class="col-2 justify-content-center" style="display: flex;">
+            <form method="POST" enctype="multipart/form-data" action="{{ route('dashboard.products.photos.destroy',[$photo->product_id,$photo->id]) }}" accept-charset="UTF-8">
+                @method('DELETE')
+                @csrf
+                <input class="form-control" type="hidden" name="photo_id" value="{!!$photo->id!!}">
+                <button type="submit" class="btn btn-danger">Usuń zdjęcie</button></a>
+            </form>
         </div>
     @endforeach
 </div>

0ad7f09ae0908654c9b60921ad93c246b0fd259d
DKlemczak <73554778+DKlemczak@users.noreply.github.com>
2021-06-09 19:32:17 +0200

    Dodanie nieszczęsnej rki

--- website/routes/api.php
+++ website/routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('ApiToken')->group(function ()
     Route::get('/orders', 'App\Http\Controllers\Api\OrdersController@index');
     Route::post('/orders/setstatus','App\Http\Controllers\Api\OrdersController@setStatus');
     Route::post('/products','App\Http\Controllers\Api\ProductsController@index');
-    Route::post('/productsgroup','App\Http\Controlles\Api\ProductsGroupController@index');
+    Route::post('/productsgroup','App\Http\Controllers\Api\ProductsGroupController@index');
     Route::post('/user/setenovacode','App\Http\Controllers\Api\UserController@setenovacode');
 });
